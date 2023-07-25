a67bf7d720ebb2577d5ecec4baf05520bcb9cf89
YusronAi <yus777999777@gmail.com>
2023-07-25 08:47:44 +0700

    Belajar MVC 'Routing dan Controller'

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class about {
+
+    public function index ($nama = "Yusron", $pekerjaan = "Progammer"){
+        echo "Saya adalah $nama, dan pekerjaan adalah $pekerjaan";
+    }
+
+    public function page (){
+        echo "Ini method page";
+    }
+}
